1670b1037d843d305a7c1b2a2963aabe9d9b39de
peterhartman417 <peterhartman@protonmail.com>
2026-03-13 18:39:37 +0000

    Deploying to gh-pages from @ peterhartman417/airstream.ing@93c9ca7f44a4c2a8dbc44764717099bfba2addc9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,14 +469,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%50%65%74%65%72%61%6E%64%43%61%6C%65%65@%61%69%72%73%74%72%65%61%6D.%69%6E%67", "_blank");
         },
       },{
         id: 'social-rss',
@@ -486,18 +479,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.airforums.com/forums/airstream-trailer-forums.1373/", "_blank");
         },
       },{
       id: 'light-theme',
